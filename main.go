1e630c84393c01e1af2b36f35369745266a2e7f1
Dorian Heinrichs <dorian.heinrichs@xiamli.com>
2023-09-05 10:06:18 +0200

    Added option to use PTA
    + -q/-qq flags for quiet/quieter output
    + -u/-user flag as alternative to --type user

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -19,21 +19,26 @@ type Repository struct {
 }
 
 var (
-	account     string
-	accountType string
-	outputDir   string
-	startPage   int
-	perPage     int
-	verbose     bool
-	noPrompt    bool
-	all         bool
-	dryRun      bool
-	totalCloned int
+	account             string
+	accountType         string
+	userType            bool
+	outputDir           string
+	startPage           int
+	perPage             int
+	verbose             bool
+	quiet               bool
+	quieterQuiet        bool
+	noPrompt            bool
+	all                 bool
+	dryRun              bool
+	personalAccessToken string
 )
 
 func main() {
 	flag.StringVar(&accountType, "type", "org", "What type of GitHub account to clone, either \"org\" or \"user\"")
 	flag.StringVar(&accountType, "t", "org", "What type of GitHub account to clone, either \"org\" or \"user\"")
+	flag.BoolVar(&userType, "user", false, "Alias to --type user")
+	flag.BoolVar(&userType, "u", false, "Alias to --type user")
 	flag.StringVar(&outputDir, "output", "AccountName", "The directory to clone the repos to")
 	flag.StringVar(&outputDir, "o", "AccountName", "The directory to clone the repos to")
 	flag.IntVar(&startPage, "page", 1, "The page number to start cloning from")
@@ -42,12 +47,19 @@ func main() {
 	flag.IntVar(&perPage, "pp", 100, "The number of entities to clone per page")
 	flag.BoolVar(&verbose, "verbose", false, "Whether to print verbose output")
 	flag.BoolVar(&verbose, "v", false, "Whether to print verbose output")
+	flag.BoolVar(&quiet, "quiet", false, "Whether to suppress unnecessary, informational, output")
+	flag.BoolVar(&quiet, "q", false, "Whether to suppress unnecessary, informational, output")
+	flag.BoolVar(&quieterQuiet, "quieter-quiet", false, "Whether to suppress all output. Errors will still be printed")
+	flag.BoolVar(&quieterQuiet, "qq", false, "Whether to suppress all output. Errors will still be printed")
 	flag.BoolVar(&noPrompt, "no-prompt", false, "With this flag enabled, there are no interactive prompts")
 	flag.BoolVar(&noPrompt, "np", false, "With this flag enabled, there are no interactive prompts")
 	flag.BoolVar(&all, "all", false, "With this flag enabled, all pages will be cloned without prompting")
 	flag.BoolVar(&all, "a", false, "With this flag enabled, all pages will be cloned without prompting")
 	flag.BoolVar(&dryRun, "dry-run", false, "With this flag enabled, no repos will be cloned")
+	flag.BoolVar(&dryRun, "dry", false, "With this flag enabled, no repos will be cloned")
 	flag.BoolVar(&dryRun, "dr", false, "With this flag enabled, no repos will be cloned")
+	flag.StringVar(&personalAccessToken, "token", "", "GitHub Fine-grained Token to use for authentication\nNeeds Repository->Metadata->Read-Only and Repository->Contents->Read-Only permissions (latter not when dry-run is enabled)")
+	flag.StringVar(&personalAccessToken, "pat", "", "GitHub Personal Access Token to use for authentication\nNeeds full repo access permissions. Use of fine-grained tokens are recommended over this")
 
 	checkVars()
 	clone(startPage)
@@ -56,22 +68,26 @@ func main() {
 func checkVars() {
 	flag.Parse()
 
+	if quieterQuiet {
+		quiet = true
+	}
+
 	if accountType != "org" && accountType != "user" {
-		fmt.Println("Invalid account type provided")
+		fmt.Fprint(os.Stderr, "Invalid account type provided")
 		os.Exit(1)
-	} else if accountType == "org" {
+	} else if accountType == "org" && !userType {
 		accountType = "orgs"
-	} else if accountType == "user" {
+	} else if accountType == "user" || userType {
 		accountType = "users"
 	}
 
 	if startPage < 1 {
-		fmt.Println("Invalid page number provided")
+		fmt.Fprint(os.Stderr, "Invalid page number provided")
 		os.Exit(1)
 	}
 
 	if perPage < 1 {
-		fmt.Println("Invalid items number provided")
+		fmt.Fprint(os.Stderr, "Invalid items number provided")
 		os.Exit(1)
 	}
 
@@ -83,6 +99,19 @@ func checkVars() {
 
 	if outputDir == "AccountName" {
 		outputDir = account
+	}
+
+	if personalAccessToken != "" && !CheckToken(personalAccessToken) {
+		fmt.Fprint(os.Stderr, "Invalid token provided")
+		os.Exit(1)
+	} else if personalAccessToken == "" && !quieterQuiet {
+		fmt.Println("No personal access token provided. This may result in rate limiting. Check out token/pat flag using --help to find out more.")
+	}
+}
+
+func printConfig() {
+	if quiet {
+		return
 	}
 
 	// Print current configuration
@@ -123,50 +152,107 @@ func checkVars() {
 			config.WriteString("enabled.")
 		}
 	}
+
 	fmt.Println(config.String())
 }
 
+var (
+	hasRetried  = false
+	firstRun    = true
+	totalCloned int
+)
+
+//goland:noinspection GoUnhandledErrorResult
 func clone(page int) {
+	if firstRun {
+		printConfig()
+	}
 	requestUrl := fmt.Sprintf("https://api.github.com/%s/%s/repos?page=%d&per_page=%d", accountType, account, page, perPage)
 	if verbose {
 		fmt.Printf("Request URL: %s\n", requestUrl)
 	}
-	resp, err := http.Get(requestUrl)
-	if err != nil || resp.StatusCode != 200 {
-		println("Error making request to GitHub API")
-		panic(err)
+	req, _ := http.NewRequest("GET", requestUrl, nil)
+	if personalAccessToken != "" {
+		req.Header.Set("Authorization", "Bearer "+personalAccessToken)
 	}
+	req.Header.Set("User-Agent", "d0x7/github-clone")
+	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
+	resp, err := http.Get(requestUrl)
+
+	if err != nil || resp.StatusCode != 200 {
+		// Rate limit exceeded
+		if resp.StatusCode == 403 && req.Header.Get("X-Ratelimit-Remaining") == "0" {
+			fmt.Fprintf(os.Stderr, "Rate limit exceeded. Please try %s.\n", GetRateLimitReset(resp))
+			if personalAccessToken == "" {
+				fmt.Fprint(os.Stderr, "Authenticated users have a higher rate limit (5000 instead of 60 requests per hour). Please consider using a token.")
+			}
+
+			os.Exit(2)
+		}
+
+		// Account not found
+		if resp.StatusCode == 404 {
+			if noPrompt {
+				fmt.Fprint(os.Stderr, "Account not found. Please ensure it is spelled correctly, because it's neither a user nor an organization.\n")
+				os.Exit(2)
+			}
+
+			if hasRetried {
+				fmt.Fprint(os.Stderr, "Account still not found. Please ensure it is spelled correctly, because it's neither a user nor an organization.\n")
+				os.Exit(2)
+			}
+
+			oppositeType := "users"
+			currentType := "User"
+			if accountType == "users" {
+				oppositeType = "orgs"
+				currentType = "Organization"
+			}
+
+			fmt.Fprintf(os.Stderr, "%s not found. Please ensure it is spelled correctly and that it is the correct account type.\n", currentType)
+			fmt.Printf("Do you wanna try again using the %s account type? (y/n) ", strings.ToLower(currentType))
+
+			if ReadYesNo() {
+				hasRetried = true
+				accountType = oppositeType
+				clone(page)
+				return
+			} else {
+				if !quieterQuiet {
+					fmt.Println("Exiting...")
+				}
+				os.Exit(2)
+			}
+		}
+
+		if err != nil {
+			println("Error making request to GitHub API:", err.Error())
+		}
+		os.Exit(1)
+	}
+
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-			println("Error closing response body")
-			panic(err)
+			fmt.Printf("Error closing response body: %s\n", err.Error())
+			os.Exit(1)
 		}
 	}(resp.Body)
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		println("Error reading response body")
-		panic(err)
-	}
-
 	var repos []Repository
-	if err := json.Unmarshal(body, &repos); err != nil {
-		println("Error unmarshalling response body")
-		panic(err)
+	err = json.NewDecoder(resp.Body).Decode(&repos)
+	if err != nil {
+		fmt.Printf("Error decoding response body: %s\n", err.Error())
+		os.Exit(1)
 	}
-
 	if !all {
 		fmt.Printf("Cloning %d repositories from %s starting from page %d…\n", len(repos), account, page)
 	}
-
 	if !dryRun && outputDir != "." {
-		err := os.MkdirAll(outputDir, 0755)
+		err := os.MkdirAll(outputDir, 0o755)
 		if err != nil {
 			fmt.Printf("Error creating output directory %s: %s\n", outputDir, err.Error())
 		}
 	}
-
 	var waitGroup sync.WaitGroup
 	for _, repo := range repos {
 		if verbose {
@@ -180,24 +266,27 @@ func clone(page int) {
 		}(repo)
 	}
 	waitGroup.Wait()
+	firstRun = false
 
 	if len(repos) == perPage {
-		fmt.Printf("\nCloned %d repositories from %s!\n\n", len(repos), account)
+		if !quiet {
+			fmt.Printf("\nCloned %d repositories from %s!\n\n", len(repos), account)
+		}
 		if all {
-			fmt.Printf("Cloning page %d of %s\n", page+1, account)
+			if !quiet {
+				fmt.Printf("Cloning page %d of %s\n", page+1, account)
+			}
 			clone(page + 1)
 		} else if noPrompt {
-			fmt.Println("There seem to be more repos, but interactive prompts are disabled.")
+			if !quieterQuiet {
+				fmt.Println("There seem to be more repos, but interactive prompts are disabled.")
+			}
 		} else {
 			fmt.Printf("Continue cloning on page %d of %s? (Y/n): ", page+1, account)
-			read, err := bufio.NewReader(os.Stdin).ReadString('\n')
-			if err != nil {
-				panic(err)
-			}
-			read = strings.ToLower(strings.TrimSpace(read))
-
-			if read == "" || read == "y" || read == "yes" {
-				fmt.Printf("Cloning page %d of %s\n", page+1, account)
+			if ReadYesNo() {
+				if !quiet {
+					fmt.Printf("Cloning page %d of %s\n", page+1, account)
+				}
 				clone(page + 1)
 			}
 		}
@@ -206,7 +295,9 @@ func clone(page int) {
 	if all {
 		allString = "all "
 	}
-	fmt.Printf("Done cloning %s%d repos from %s. Bye bye!\n", allString, totalCloned, account)
+	if !quiet {
+		fmt.Printf("Done cloning %s%d repos from %s. Bye bye!\n", allString, totalCloned, account)
+	}
 	os.Exit(0)
 }
 
@@ -232,6 +323,17 @@ func cloneRepo(repo Repository) {
 		cmd.Stderr = os.Stderr
 	}
 	err := cmd.Run()
+
+	// Check if err is of type ExitError
+	// This is the only way to check if the command exited with a non-zero exit code
+	// https://stackoverflow.com/a/10385867
+
+	var exitError *exec.ExitError
+	if errors.As(err, &exitError) && exitError.ExitCode() == 128 {
+		fmt.Printf("Skipping %s because it already exists\n", repo.Name)
+		return
+	}
+
 	if err != nil {
 		fmt.Printf("Error cloning repository %s: %s\n", repo.Name, err.Error())
 	}
